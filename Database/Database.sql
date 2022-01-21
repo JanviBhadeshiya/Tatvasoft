f6a7fe264bae2f304bf98148ea82fda32fc47438
JanviBhadeshiya <94890542+JanviBhadeshiya@users.noreply.github.com>
2022-01-21 16:25:02 +0530

    Update Database.sql

--- Database/Database.sql
+++ Database/Database.sql
@@ -1,27 +1,6 @@
--- phpMyAdmin SQL Dump
--- version 5.1.1
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Jan 21, 2022 at 09:20 AM
--- Server version: 10.4.21-MariaDB
--- PHP Version: 8.0.11
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
 --
 -- Database: `helperland`
 --
-
-
 
 CREATE TABLE `for_customer` (
   `Customer_id` int(11) NOT NULL,
