a6035c89ba13a7b4de48e9b701251e7697ec5354
JanviBhadeshiya <94890542+JanviBhadeshiya@users.noreply.github.com>
2022-01-28 15:34:32 +0530

    Add files via upload

---
+++ Helperland/view/prices.php
@@ -0,0 +1,237 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Prices</title>
+    <link
+			href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
+			rel="stylesheet"
+			integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
+			crossorigin="anonymous"
+		/>
+    <link rel="stylesheet" href="../assets/css/prices-style.css">
+</head>
+<body>
+    <nav class="d-flex justify-content-between align-items-center text-white">
+        <div class="nav-block">
+            <div class="header-minilogo">
+                <a class="d-block" href="index.html">
+                    <img src="../assets/img/logo-small.png" alt="logo" />
+                </a>
+                <div class="hamburger h1 me-3"></div>
+            </div>
+            <div class="responsive_nav">
+                <button id="booknow" href="#">Book now</button>
+                <button id="priceandservice" href="#">Prices & service</button>
+                <a class="header-navbar" href="#">Warranty</a>
+                <a class="header-navbar" href="#">Blog</a>
+                <a class="header-navbar" href="#">Contact</a>
+                <button id="loginbttn" class="sp-btn">Login</button>
+                <button id="becomehelper" class="sp-btn">Become a Helper</button>
+          </div>
+        </div>
+    </nav>
+    <!-- bgimg of 3person -->
+    <section class="faq-banner">
+        <img src="../assets/img/group-18.png" alt="faqHeaderImage" style="max-width:100%;height:auto;">
+    </section>
+    <!--prices one time-->
+    <section>
+        <div class="flex-column d-flex align-items-center justify-content-center">
+            <div class="cleaningHeader1">Prices</div>
+            <div class="faHeaderDecoration w-100 d-flex align-items-center justify-content-center">
+                <span></span>
+              <img src = "../assets/img/forma-1-copy-5.png"/>
+                <span></span>
+            </div>
+        </div>
+        <div class="priceCard">
+            <div class="priceCardHeader text-center">One Time</div>
+            <div class="priceCardText d-flex align-items-center justify-content-center">€ 20/hr</div>
+            <div class="priceCardDescription row g-2">
+                <div class="priceCardDescriptionItem col-11 col-sm-8 col-md-7">
+                    <img src="../assets/img/forma-1_5.png" alt="tickMark" /> Lower prices
+                </div>
+                <div class="priceCardDescriptionItem col-11 col-sm-8 col-md-7">
+                    <img src="../assets/img/forma-1_5.png" alt="tickMark" /> Easy online & secure payment
+                </div>
+                <div class="priceCardDescriptionItem col-11 col-sm-8 col-md-7">
+                    <img src="../assets/img/forma-1_5.png" alt="tickMark" /> Easy amendment
+                </div>
+            </div>
+        </div>
+        <center><hr class="w-75" /></center>
+    </section>
+    <!-- extra service section -->
+    <section>
+        <div class="flex-column d-flex align-items-center justify-content-center">
+            <div class="cleaningHeader1">Extra services</div>
+            <div class="faHeaderDecoration w-100 d-flex align-items-center justify-content-center">
+                <span></span>
+                <img src="../assets/img/forma-1-copy-5.png"/>
+                <span></span>
+            </div>
+            <div class="extra-services-5">
+                <div class="extra-services-temp">
+                    <div class="serviceImage rounded-circle d-flex align-items-center justify-content-center">
+						<img src="../assets/img/3.png" alt="InsideCabinets" />
+					</div>
+                    <center><p class="imgtext">Inside cabinets</p></center>
+                    <center><p class="txtblue">30 minutes</p></center>
+                </div>
+                <div class="extra-services-temp">
+                    <div class="serviceImage rounded-circle d-flex align-items-center justify-content-center">
+						<img src="../assets/img/5.png" alt="InsideCabinets" />
+					</div>
+                    <center><p class="imgtext">Inside cabinets</p></center>
+                    <center><p class="txtblue">30 minutes</p></center>
+                </div>
+                <div class="extra-services-temp">
+                    <div class="serviceImage rounded-circle d-flex align-items-center justify-content-center">
+						<img src="../assets/img/4.png" alt="InsideCabinets" />
+					</div>
+                    <center><p class="imgtext">Inside cabinets</p></center>
+                    <center><p class="txtblue">30 minutes</p></center>
+                </div>
+                <div class="extra-services-temp">
+                    <div class="serviceImage rounded-circle d-flex align-items-center justify-content-center">
+						<img src="../assets/img/2.png" alt="InsideCabinets" />
+					</div>
+                    <center><p class="imgtext">Inside cabinets</p></center>
+                    <center><p class="txtblue">30 minutes</p></center>
+                </div>
+                <div class="extra-services-temp">
+                    <div class="serviceImage rounded-circle d-flex align-items-center justify-content-center">
+						<img src="../assets/img/1.png" alt="InsideCabinets" />
+					</div>
+                    <center><p class="imgtext">Inside cabinets</p></center>
+                    <center><p class="txtblue">30 minutes</p></center>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- 3 photo section -->
+    <section id="greysec">
+        <div class="cleaningHeader"><center>What we include in cleaning</center></div>
+            <div class="faHeaderDecoration w-100 d-flex align-items-center justify-content-center">
+                <span></span>
+              
+                <img src = "../assets/img/forma-1-copy-5.png"/>
+                <span></span>
+            </div>
+        <div class="img3">
+            <div class="divs3">
+                <img src="../assets/img/group-18_3.png" alt="homeimg">
+                <h5>Bedroom and Living Room</h5>
+                <div class="texts"><img src="../assets/img/right-arrow-grey.png" class="arrow" alt="arrow">Dust all accessible surfaces</div>
+                <div class="texts"><img src="../assets/img/right-arrow-grey.png" class="arrow" alt="arrow">Wipe down all mirrors and glass fixtures</div>
+                <div class="texts"><img src="../assets/img/right-arrow-grey.png" class="arrow" alt="arrow">Clean all floor surfaces</div> 
+                <div class="texts"><img src="../assets/img/right-arrow-grey.png" class="arrow" alt="arrow">Take out garbage and recycling</div>
+            </div>
+            <div class="divs3">
+                <img src="../assets/img/group-18_4.png" alt="homeimg">
+                <h5>Bathrooms</h5>
+                <div class="texts"><img src="../assets/img/right-arrow-grey.png" class="arrow" alt="arrow">Wash and sanitize the toilet, shower, tub, sink</div>
+                <div class="texts"><img src="../assets/img/right-arrow-grey.png" class="arrow" alt="arrow">Dust all accessible surfaces</div>
+                <div class="texts"><img src="../assets/img/right-arrow-grey.png" class="arrow" alt="arrow">Wipe down all mirrors and glass fixtures</div>
+                <div class="texts"><img src="../assets/img/right-arrow-grey.png" class="arrow" alt="arrow">Clean all floor surfaces</div> 
+                <div class="texts"><img src="../assets/img/right-arrow-grey.png" class="arrow" alt="arrow">Take out garbage and recycling</div>
+            </div>
+            <div class="divs3">
+                <img src="../assets/img/group-18_2.png" alt="homeimg">
+                <h5>Kitchen</h5>
+                <div class="texts"><img src="../assets/img/right-arrow-grey.png" class="arrow" alt="arrow">Dust all accessible surfaces</div>
+                <div class="texts"><img src="../assets/img/right-arrow-grey.png" class="arrow" alt="arrow">Empty sink and load up dishwasher</div>
+                <div class="texts"><img src="../assets/img/right-arrow-grey.png" class="arrow" alt="arrow">Wipe down all mirrors and glass fixtures</div>
+                <div class="texts"><img src="../assets/img/right-arrow-grey.png" class="arrow" alt="arrow">Clean all floor surfaces</div> 
+                <div class="texts"><img src="../assets/img/right-arrow-grey.png" class="arrow" alt="arrow">Take out garbage and recycling</div>
+            </div>
+        </div>
+    </section>
+
+    <!-- why helperhand -->
+    <section>
+        <div class="flex-column d-flex align-items-center justify-content-center">
+            <div class="cleaningHeader1">Why Helperland</div>
+            <div class="faHeaderDecoration w-100 d-flex align-items-center justify-content-center">
+                <span></span>
+                <img src="../assets/img/forma-1-copy-5.png"/>
+                <span></span>
+            </div>
+        </div>
+        <div class="whyHelperland3">
+            <div>
+                <div class="whyDescriptionItem">
+                    <div class="whyDescriptionItemHeader text-center text-sm-center text-md-center text-lg-end">
+                        Experienced and vetted professionals
+                    </div>
+                    <div class="whyDescriptionItemText text-center text-sm-center text-md-center text-lg-end">
+                        dominate the industry in scale and scope with an adaptable, extensive network that consistently delivers exceptional
+                        results.
+                    </div>
+                </div>
+                <div class="whyDescriptionItem">
+                    <div class="whyDescriptionItemHeader text-center text-sm-center text-md-center text-lg-end">Dedicated customer service</div>
+                    <div class="whyDescriptionItemText text-center text-sm-center text-md-center text-lg-end">
+                        to our customers and are guided in all we do by their needs. The team is always happy to support you and offer all the
+                        information. you need.
+                    </div>
+                </div>
+            </div>
+            <div><img src="../assets/img/the-best-img-1.png" alt="the-best-img-1.png"></div>
+            <div>
+                <div class="whyDescriptionItem">
+                    <div class="whyDescriptionItemHeader text-center text-sm-center text-md-center text-lg-start">Every cleaning is insured</div>
+                    <div class="whyDescriptionItemText text-center text-sm-center text-md-start">
+                        and seek to provide exceptional service and engage in proactive behavior. We‘d be happy to clean your homes.
+                    </div>
+                </div>
+                <div class="whyDescriptionItem">
+                    <div class="whyDescriptionItemHeader text-center text-sm-center text-md-center text-lg-start">Secure online payment</div>
+                    <div class="whyDescriptionItemText text-center text-sm-center text-md-start">
+                        Payment is processed securely online. Customers pay safely online and manage the booking.
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!--footer-->
+    <section class="testimonial mt-5">
+        <div class="container-lg">
+            <div class="bottom-area">
+                <div class="bot-form">
+                    <div class="layer">
+                        <a href="#">
+                            <img src="../assets/img/layer-598.png" alt="">
+                        </a>
+                    </div>
+                </div>
+                <div class="newsletter text-center">
+                    <h2>GET OUR NEWSLETTER</h2>
+                    <form action="#">
+                        <input type="text" class="form-email" placeholder="YOUR EMAIL">
+                        <button type="submit" class="btn-submit">Submit</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </section>
+    <?php include('footer.php'); ?>
+  
+    <div class="home-btn">
+        <a href="#">
+            <div class="up-arrow">
+              <center>
+              <img id="last-icon" src="../assets/img/forma-1.png" alt="forma-1.png">
+              </center>
+            </div>
+        </a>
+    </div>
+    <script src=".\js\bootstrap.bundle.min.js"></script>
+</body>
+
+</html>
